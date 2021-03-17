a4d311a0ff7e2dbe1ad695255c0baaa17554ff4b
Gregor Betz <gregor.betz@kit.edu>
2021-03-17 14:25:40 +0100

    Update beldynlm.py

--- beldynlm.py
+++ beldynlm.py
@@ -391,7 +391,7 @@ class ListeningLMAgent(AbstractLMAgent,LMUtilitiesMixIn):
         peer_posts = [p for p in peer_posts if not p in perspective] # exclude posts already in perspective
 
         # DEBUG
-        print('Agent {}: perspective = {} ({}), peer posts = {} ().'.format(self.agent, len(perspective), len(set(perspective)), len(peer_posts), len(set(peer_posts))))
+        print('Agent {}: perspective = {} ({}), peer posts = {} ({}).'.format(self.agent, len(perspective), len(set(perspective)), len(peer_posts), len(set(peer_posts))))
         
 
         # determine weights for selecting new posts for perspective according to perspective_expansion_method
