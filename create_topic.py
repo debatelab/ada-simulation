5acf84ee5eccca3beb12ee24fcc3ff0d0768d890
Gregor Betz <gregor.betz@kit.edu>
2021-03-19 09:06:52 +0100

    topics
    50/50 with conclusions

--- create_topic.py
+++ create_topic.py
@@ -79,10 +79,12 @@ for template in templates:
             else:
                 val = list({'pro','con'}-{parent_val})[0]
             valence_dict[line_split[0]] = val
+            conclusion = ""
+            if random.choice([True, False]):
+                conclusion = random.choice([' So', ' I think', ' I believe', " That's why"]) + random.choice(template.get(val))
             text = (
                 " ".join(line_split[2:]) + 
-                random.choice([' So', ' I think', ' I believe', " That's why"]) + 
-                random.choice(template.get(val))
+                conclusion
             )               
             new_topic['initial_posts'].append({'type':val,'text':text})
     
